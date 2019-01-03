d9ebddfacb4d2daaef250696856acd10a42304b1
Ania <ana.breslavska@gmail.com>
2019-01-03 11:53:23 +0000

    HVAC skeleton

---
+++ hvac-service/src/main/java/com/cit/masters/hvacservice/HvacServiceApplication.java
@@ -0,0 +1,16 @@
+package com.cit.masters.hvacservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+
+@SpringBootApplication
+public class HvacServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HvacServiceApplication.class, args);
+    }
+}

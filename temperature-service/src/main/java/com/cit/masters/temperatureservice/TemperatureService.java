e73b6d062af8adc759b341109147c14b7cd1f403
Ania <ana.breslavska@gmail.com>
2019-01-03 11:03:50 +0000

    Working Climate Control and Temperature version v2

--- temperature-service/src/main/java/com/cit/masters/temperatureservice/TemperatureService.java
+++ temperature-service/src/main/java/com/cit/masters/temperatureservice/TemperatureService.java
@@ -2,6 +2,7 @@ package com.cit.masters.temperatureservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,14 +16,21 @@ public class TemperatureService {
     public Boolean process(TemperatureData data) {
 
         // TODO: add UUID normal check
+        // TODO: remove "service" from package names
         if (data.getTemperature() < 15 || data.getTemperature() > 25) {
             log.info("Temperature for sensor {} is not in a normal range: {}", data.getSensorId(), data.getTemperature());
 
             RestTemplate restTemplate = new RestTemplate();
             HttpEntity<TemperatureData> request = new HttpEntity<>(data);
-            TemperatureData temperatureData = restTemplate.postForObject("http://localhost:8083", request, TemperatureData.class);
+            try {
+                // TODO all internal requests route via Gateway
+                ResponseEntity<String> entity = restTemplate.postForEntity("http://localhost:8083", request, String.class);
+                log.info("Response status: {}", entity.getStatusCodeValue());
+                return true;
+            } catch (Exception ex) {
+                return false;
+            }
 
-            return true;
         } else {
             log.info("Temperature for sensor {} is in a normal range: {}", data.getSensorId(), data.getTemperature());
             return true;

c8b5e6dfa5a665770a2e7df1dcbbfa850a1b01be
Ania <ana.breslavska@gmail.com>
2019-01-03 12:24:03 +0000

    alert service skeleton v4

--- fire-service/src/main/java/com/cit/masters/fireservice/FireService.java
+++ fire-service/src/main/java/com/cit/masters/fireservice/FireService.java
@@ -1,22 +1,32 @@
 package com.cit.masters.fireservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import static java.util.UUID.fromString;
+import org.springframework.web.client.RestTemplate;
 
 /**
  *
  */
 @Service
+@Slf4j
 public class FireService {
 
     public Boolean receive(AssetClimateData data) {
 
+        // TODO: add proper validation for UUID and such
+        log.info("Fire service: Fire alert received, temperature is {}", data.getTemperature());
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpEntity<AssetClimateData> request = new HttpEntity<>(data);
         try {
-            fromString(data.getSensorId());
+            ResponseEntity<String> entity = restTemplate.postForEntity("http://localhost:8086", request, String.class);
+            log.info("Response status: {}", entity.getStatusCodeValue());
             return true;
-        } catch (IllegalArgumentException exception) {
+        } catch (Exception ex) {
             return false;
+
         }
     }
 }

145d2ba9c8b651d05c7b19a98f7f05c2f1ae2942
Ania <ana.breslavska@gmail.com>
2019-01-03 10:05:47 +0000

    Climate control skeleton

---
+++ climate-control-service/src/main/java/com/cit/masters/climateservice/ClimateServiceApplication.java
@@ -0,0 +1,12 @@
+package com.cit.masters.climateservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimateServiceApplication.class, args);
+    }
+}

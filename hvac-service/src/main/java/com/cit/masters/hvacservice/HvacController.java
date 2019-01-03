eee5124b642c1db4ca4eeb127176fdbd2f0fb6f6
Ania <ana.breslavska@gmail.com>
2019-01-03 12:02:47 +0000

    HVAC skeleton v4

--- hvac-service/src/main/java/com/cit/masters/hvacservice/HvacController.java
+++ hvac-service/src/main/java/com/cit/masters/hvacservice/HvacController.java
@@ -1,15 +1,32 @@
 package com.cit.masters.hvacservice;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
 
 /**
  *
  */
 @RestController
+@Slf4j
 public class HvacController {
 
     @Resource
     private HvacService hvacService;
+
+    @PostMapping
+    public ResponseEntity<Object> create(@RequestBody @Valid AssetClimateData data) {
+        log.info("Incoming Temperature request to HVAC Controller: {}", data);
+        boolean received = hvacService.receive(data);
+        if (!received) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
 }
--- hvac-service/src/main/java/com/cit/masters/hvacservice/HvacService.java
+++ hvac-service/src/main/java/com/cit/masters/hvacservice/HvacService.java
@@ -2,10 +2,22 @@ package com.cit.masters.hvacservice;
 
 import org.springframework.stereotype.Service;
 
+import static java.util.UUID.fromString;
+
 /**
  *
  */
 
 @Service
 public class HvacService {
+
+    public Boolean receive(AssetClimateData data) {
+
+        try {
+            fromString(data.getSensorId());
+            return true;
+        } catch (IllegalArgumentException exception) {
+            return false;
+        }
+    }
 }
